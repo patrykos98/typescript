0c51a712fdd57bd39f1f7ab6cbf4391a4ab364fa
Patryk Szczemczak <patrykszczemczak@wp.pl>
2023-12-29 10:54:20 +0100

    proste inputy funkcje typescript

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,8 @@
-console.log('test');
+const input1 = document.querySelector('#input1');
+const input2 = document.querySelector('#input2');
+const btn = document.querySelector('button');
+const add = (v1, v2) => v1 + v2;
+btn.addEventListener("click", () => {
+    const sum = add(Number(input1.value), Number(input2.value));
+    console.log(sum);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log('test');
+const input1: HTMLInputElement=document.querySelector('#input1');
+const input2: HTMLInputElement=document.querySelector('#input2');
+const btn=document.querySelector('button');
+
+const add=(v1: number, v2: number)=>v1 + v2;
+
+btn.addEventListener("click",()=>{
+    const sum=add(Number(input1.value),Number(input2.value));
+    console.log(sum);
+})
